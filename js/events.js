8bab2ff100af00546acced81eb4245fd7cab3765
Victor <victor.y.kung@gmail.com>
2014-07-06 14:57:41 -0500

    finished up designing the events index page

--- js/events.js
+++ js/events.js
@@ -1,3 +1,5 @@
+// Homepage Changing Background
+
 var currentBackground = 0;
 var backgrounds = [];
 backgrounds[0] = '/Users/victorkung/Coding/Projects/UEvents-Static/img/bg1.jpg';
@@ -23,4 +25,21 @@ function changeBackground() {
 
 $(document).ready(function() {
 	setTimeout(changeBackground, 6000);        
+
+	// Sticky Header
+	// $(window).scroll(function() {
+	// 	var scrollFromTop = $(this).scrollTop() + 50;
+	// 	return $(".event-day").each(function() {
+	// 		var offsetTop = $(this).offset().top;
+	// 		var wrapper = $(this).parent();
+	// 		var wrapperOffsetTop = wrapper.offset().top;
+	// 		var topPosition = scrollFromTop - wrapperOffsetTop;
+	// 		if (topPosition < 0) {
+	// 			topPosition = 0;
+	// 		}
+	// 		$(this).css("top", topPosition);
+	// 	});
+	// });
+	
 });
+
